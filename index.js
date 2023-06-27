dd705742f37bb53993216e7ab34bd047a647d11e
Carlos-Arizmendi <115851632+Carlos-Arizmendi@users.noreply.github.com>
2023-06-27 17:54:41 -0500

    crear documento html de cada opcion del menu
    se realizo cada parte del menu y se realizo cada enlace con cada documento html

---
+++ index.js
@@ -0,0 +1,30 @@
+<<<<<<< Updated upstream
+const endpoint=`https://www.thecocktaildb.com/api/json/v1/1/search.php?page=${page}`
+
+
+function paginationtrapper(info){
+    let template= ``
+    for (let index = 1; index <= info.pages; index++) {
+        template+=`
+        <div class="page">
+        <a href="index.html?page=${index}">${index}</>
+        </div>
+        ` 
+    }
+    pagination.innerHTML= template
+=======
+const endpoint=`https://www.thecocktaildb.com/api/json/v1/1/search.php?page=${page}`
+
+
+function paginationtrapper(info){
+    let template= ``
+    for (let index = 1; index <= info.pages; index++) {
+        template+=`
+        <div class="page">
+        <a href="index.html?page=${index}">${index}</>
+        </div>
+        ` 
+    }
+    pagination.innerHTML= template
+>>>>>>> Stashed changes
+    }
