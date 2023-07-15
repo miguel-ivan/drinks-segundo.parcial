58cb0f571b1555b8a45b8c4bfafa17d8f2911cdf
miguel-ivan <miguelivanchancastillo@gmail.com>
2023-07-15 03:31:48 -0500

    puestos los cocteles en columnas de 3

--- JSON/catalogo.js
+++ JSON/catalogo.js
@@ -1,55 +1,25 @@
 async function fetching(){
     const url= window.location.search
     const urlParans= new URLSearchParams(url)
-    const letter= urlParans.get("letter")|| a
+    const letter= urlParans.get("letter")|| "a"
 
-    const endpoint=`https://www.thecocktaildb.com/api/json/v1/1/search/${letter}`
+    const endpoint=`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`
 
     const resonse = await fetch (endpoint)
     const json = await resonse.json()
     return json
 }
+fetching().then(
+  response => {
+    const drinks = response.drinks
+    let template = ''
+    drinks.forEach(drink => {
+      template += `
+             <img src=${drink.strDrinkThumb}>
+             <h2>${drink.strDrink}</h2>
+             `
+    })
+    results.innerHTML = template
+  }
+)
 
-let getInfo = () => {
-    fetch(url + "&lang=es")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.drinks);
-        let myDrink = data.drinks[0];
-        let count = 1;
-        let ingredients = [];
-        for (let i in myDrink) {
-          let ingredient = "";
-          let measure = "";
-          if (i.startsWith("strIngredient") && myDrink[i]) {
-            ingredient = myDrink[i];
-            if (myDrink[`strMeasure` + count]) {
-              measure = myDrink[`strMeasure` + count];
-            } else {
-              measure = "";
-            }
-            count += 1;
-            ingredients.push(`${measure} ${ingredient}`);
-          }
-        }
-        result.innerHTML = `
-      <img src=${myDrink.strDrinkThumb}>
-      <h2>${myDrink.strDrink}</h2>
-      <h3>Ingredients:</h3>
-      <ul class="ingredients"></ul>
-      <h3>Instructions:</h3>
-      <p>${myDrink.strInstructions}</p>
-      `;
-        let ingredientsCon = document.querySelector(".ingredients");
-        ingredients.forEach((item) => {
-          let listItem = document.createElement("li");
-          listItem.innerText = item;
-          ingredientsCon.appendChild(listItem);
-        });
-      })
-      .catch(() => {
-        result.innerHTML = `<h3 class="msg">Por favor, introduce un valor válido</h3>`;
-      });
-};
-
-window.addEventListener("load", getInfo);
